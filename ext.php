c6c818b22adda2dde5174d11c2f1131709b98e29
cece74 <forumacquariofilia@libero.it>
2015-03-15 15:49:08 +0100

    Create ext.php

---
+++ ext.php
@@ -0,0 +1,18 @@
+<?php
+/**
+*
+* @package phpBB Extension 
+* @copyright (c) 2015 sanfi
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace sanfi\fadeheader;
+
+/**
+* @ignore
+*/
+
+class ext extends \phpbb\extension\base
+{
+}
